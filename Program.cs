96c8243ddddf7cf0e086600bba11a6d4674441b4
Gabriel Lindsay <gabelindsay9@gmail.com>
2023-10-23 10:34:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_5_Decisions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
